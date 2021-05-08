b706c962c4c49f819af0b507a9f5678b86140b7e
Kshitij Bhargav <30386003+Ajaxbhargav123@users.noreply.github.com>
2021-05-08 14:58:03 +0530

    DD

--- CoreJwt/Controllers/Auth/AuthenticationController.cs
+++ CoreJwt/Controllers/Auth/AuthenticationController.cs
@@ -31,7 +31,7 @@ namespace CoreJwt.Controllers.Auth {
                 appuser.CreatedDate=DateTime.Now;
                 await _context.AddAsync (appuser);
                 await _context.SaveChangesAsync ();
-                return Ok (appuser);
+                return Ok (201);
             } catch (Exception ex) {
                 throw ex;
             }
